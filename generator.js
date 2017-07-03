4b5676d68c0d7671942a0bd37f3af45878b07170
Shea Daniels <shea@dwolla.com>
2017-07-03 13:42:45 -0500

    Updated readme, automatically start generated server

--- generator.js
+++ generator.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const exec = require('child_process').execSync;
+const cp = require('child_process');
 const constants = require('./lib/constants');
 const thriftParser = require('./lib/thriftParser');
 const PackageGenerator = require('./lib/generators/PackageGenerator');
@@ -7,13 +7,14 @@ const ClientGenerator = require('./lib/generators/ClientGenerator');
 const ServerGenerator = require('./lib/generators/ServerGenerator');
 const ControllerGenerator = require('./lib/generators/ControllerGenerator');
 const ViewGenerator = require('./lib/generators/ViewGenerator');
+const BootstrapFileGenerator = require('./lib/generators/BootstrapFileGenerator');
 
 function getFileNameFromArgs() {
   return process.argv.slice(2)[0];
 }
 
 function generateNodeJsClient(filename, dest) {
-  exec(`thrift -r -o ${dest} --gen js:node ${filename}`);
+  cp.execSync(`thrift -r -o ${dest} --gen js:node ${filename}`);
 }
 
 function createSourceDirectory() {
@@ -21,23 +22,6 @@ function createSourceDirectory() {
       fs.mkdirSync(constants.SrcDir);
   }
 }
-
-// function writeParam(tFunc, thriftInfo) {
-//   const arg = tFunc.args[0];
-//
-//   if (arg.type === 'string') {
-//     clientStream.write(`const ${arg.name} = \'foobar\';`);
-//   } else {
-//     const argProps = thriftInfo.structs[arg.type];
-//     clientStream.write(`const ${arg.name} = new ttypes.${arg.type}();\n`);
-//
-//     argProps.forEach(prop => {
-//       if (prop.type === 'string') {
-//         clientStream.write(`${arg.name}.${prop.name} = 'foobaz';\n`);
-//       }
-//     });
-//   }
-// }
 
 const filename = getFileNameFromArgs();
 const thriftInfo = thriftParser(filename);
@@ -50,5 +34,9 @@ new ClientGenerator(thriftInfo).generate();
 new ServerGenerator(thriftInfo).generate();
 new ControllerGenerator(thriftInfo).generate();
 new ViewGenerator(thriftInfo).generate();
+new BootstrapFileGenerator().generate();
 
 console.log('Generated some stuff for you to check out in the gen-src folder!');
+
+console.log('Bootstrapping server...');
+cp.fork(`${constants.SrcDir}/bootstrap.js`, [], {cwd: `${constants.SrcDir}`});
--- readme.md
+++ readme.md
@@ -1,3 +1,26 @@
 # Stingy
 
-An automatically generated web UI for Thrift interfaces
+An automatically generated web UI for Thrift interfaces.
+
+_Current limitations:_
+This is a proof of concept. It's limited to a single parameter in each Thrift function.
+That parameter can either be a string, or an object containing string fields. More
+support for everything else to come later.
+
+## Prereqs
+
+* Apache Thrift must be installed and on your path
+* Run `npm install`
+
+## To run generator for provided example service
+
+`npm start`
+
+## To run generator for your own thrift file
+
+`node generator.js {path-to-thrift-file}`
+
+## Output
+
+The output directory is currently hardcoded to be one directory back and in a new folder:
+`../gen-src`
